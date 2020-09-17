4fab65b981e44ee5198a92119c127980373a1a38
Marce1298 <marchebordon@gmail.com>
2020-09-17 20:43:02 -0300

    Metodos1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Metodos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            mensajeEnPantalla();//un metodo se puede llamar las veces que se requiera.
+
+            Console.WriteLine("Mensaje desde el mein");
+
+            sumaNumeros(7, 9);
+
+        }
+
+
+
+        static void mensajeEnPantalla()
+        {
+
+            Console.WriteLine("Mensaje desde el método mensajeEnPantalla");
+
+
+        }
+
+
+
+        static void sumaNumeros(int num1, int num2)
+        {
+            
+            Console.WriteLine($"La suma de los numeros es: {num1 + num2} ");
+
+        }
+
+    }
+}
